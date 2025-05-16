857851c9124d8a53acf757f48b0c96c9a9327101
WoogaJJangka <siwon1004007@gmail.com>
2025-05-17 00:14:11 +0900

    Changes to be committed: 	modified:   main/game/player.py

--- main/game/player.py
+++ main/game/player.py
@@ -1,36 +1,15 @@
-<<<<<<< HEAD
-class Player:
-    def __init__(self, pid, color):
-        self.id = pid
-        self.color = color  # 'red', 'blue', etc
-        self.money = 5000
-        self.position = 0
-        self.properties = []
-        self.is_bankrupt = False
+class Player: # 플레이어 클래스
+    def __init__(self, pid, color): # 플레이어 기본 설정
+        self.color = color  # 플레이어 색깔 (red, blue, green, yellow)
+        self.money = 5000 # 시작 돈
+        self.position = 0 # 시작 위치
+        self.properties = [] # 소유한 타일
+        self.is_bankrupt = False # 파산 여부
 
-    def move(self, steps, board_size):
-        self.position = (self.position + steps) % board_size
+    def move(self, steps, board_size): # 이동 메서드
+        self.position = (self.position + steps) % board_size # 현재 위치에서 주사위의 합을 더한 후 보드 사이즈로 나눈 나머지로 이동
 
-    def pay(self, amount):
-        self.money -= amount
-        if self.money < 0:
+    def pay(self, amount): # 돈 지불
+        self.money -= amount # 현재 돈에서 지불금을 빼기
+        if self.money < 0: # 만약 현재 돈이 0보 작으면 False를 리턴
             return False
-=======
-class Player:
-    def __init__(self, pid, color):
-        self.id = pid
-        self.color = color  # 'red', 'blue', etc
-        self.money = 5000
-        self.position = 0
-        self.properties = []
-        self.is_bankrupt = False
-
-    def move(self, steps, board_size):
-        self.position = (self.position + steps) % board_size
-
-    def pay(self, amount):
-        self.money -= amount
-        if self.money < 0:
-            return False
->>>>>>> 03ef5a75298ff18032c20f31e4eb5562f738139d
-        return True
