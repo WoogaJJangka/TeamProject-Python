5462ffb16585a12c9f15ad1b92e1a9a92237e2b3
Your Name <siwon1004007@gmai.com>
2025-05-22 12:39:44 +0900

    ㅅㅄㅂ

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from roll_dices import dicemain
+from board_set import board_start 
+import roll_dices.roller
+import pygame
 
-dicemain.roll_dice()
+board_start.set_board()
+roller = roll_dices.roller.DiceRoller(board_start.background,"roll_dices\\assets")
+
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: # 키를 누르고 이벤트 키가 스페이스이면
+        roller.roll_dice()
