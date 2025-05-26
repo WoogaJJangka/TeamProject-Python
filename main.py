96895c02014fef883818abe5454d465451f6a990
Your Name <siwon1004007@gmai.com>
2025-05-26 21:11:35 +0900

    main 정리

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# from board_set import board_start
-# import roll_dices.roller
-# import pygame
-#
-# board_start.set_board()
-# roller = roll_dices.roller.DiceRoller(board_start.background,"roll_dices\\assets")
-#
-# for event in pygame.event.get():
-#     if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: # 키를 누르고 이벤트 키가 스페이스이면
-#         roller.roll_dice()
-
 import pygame
 import os
 from board_set.BoardScreen import BoardScreen
